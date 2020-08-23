58c493e6ec54c491c732bdb068f02887fb384eb7
Gleason <cgleason3462@gmail.com>
2020-08-23 12:34:24 -0500

    HW2

--- Instructions/app.js
+++ Instructions/app.js
@@ -1,13 +1,36 @@
+var dropdownMenu  = d3.select("#selDataset");
+
 function init(){
     d3.json("samples.json").then(function(data){
         console.log(data);
+
     var sample_data = data.names;
     
-    var dropDown  = d3.select("#selDataset");
     sample_data.forEach(function(name){dropdownMenu
         .append("option")
         .text(name)
         .property("value",name)})
-});
 
-};
+    // use the initial sample to have some data when we land on the page
+    var landingData = data.names[0]
+    // call function
+    filterMeta(landingData)
+        
+    
+
+})};
+// function to filter down to just the metadata
+function filterMeta(name){
+    d3.json("samples.json").then(function(data){
+    var metaData = data.metadata
+    // filtering
+    var filterMeta = metadata.id == name;
+    var demoGraphic = d3.select("#sample-metadata");
+    // Object entries
+    Object.entries(name).forEach(([key, value]) =>{
+        if (key == "")
+    }
+})};
+
+
+init();
