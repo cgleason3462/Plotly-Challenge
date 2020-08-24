bde6413cfae7613660d435771bbd9251f43206a9
Gleason <cgleason3462@gmail.com>
2020-08-23 20:20:54 -0500

    HW3

--- Instructions/app.js
+++ Instructions/app.js
@@ -12,25 +12,60 @@ function init(){
         .property("value",name)})
 
     // use the initial sample to have some data when we land on the page
-    var landingData = data.names[0]
+    var landingData = sample_data[0]
     // call function
-    filterMeta(landingData)
+    filterMeta(landingData);
+    buildCharts(sample_data);
         
     
 
 })};
-// function to filter down to just the metadata
+//function to filter down to just the metadata
 function filterMeta(name){
     d3.json("samples.json").then(function(data){
-    var metaData = data.metadata
-    // filtering
-    var filterMeta = metadata.id == name;
     var demoGraphic = d3.select("#sample-metadata");
-    // Object entries
-    Object.entries(name).forEach(([key, value]) =>{
-        if (key == "")
-    }
+    
+    demoGraphic.html("");
+    // append data to demographic info
+    // Not sure how to just append metadata
+    Object.entries(data).forEach(function ([key, value]){
+        var row = demoGraphic.append("p")
+        row.text(`${key}: ${value}`);
+    })
+   
 })};
 
+function optionChanged(sample_data){
+    filterMeta(sample_data);
+    buildCharts(sample_data);
+};
 
+function buildCharts(sample_data){
+    d3.json("samples.json").then(function(data){
+    var otu_ids = data.otu_ids;
+    var otu_labels = data.otu_labels;
+    var sample_values = data.sample_values;
+
+    //Building Bubble chart
+    var bubData = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: 'Earth'
+      }
+    }];      
+
+    var Layout = {
+      margin: { t: 0 },
+      hovermode: 'closest',
+      xaxis: {title: 'OTU ID'},
+    };
+
+    Plotly.plot('bubble', bubData, Layout);
+
+})};
 init();
