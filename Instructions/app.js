5fa54a22f3669e86e484fb9bb3217f786499014f
Gleason <cgleason3462@gmail.com>
2020-08-24 20:57:48 -0500

    final push

--- Instructions/app.js
+++ Instructions/app.js
@@ -6,6 +6,7 @@ function init(){
 
     var sample_data = data.names;
     
+    // build options for dropdown menu
     sample_data.forEach(function(name){dropdownMenu
         .append("option")
         .text(name)
@@ -34,12 +35,12 @@ function filterMeta(name){
     })
    
 })};
-
+// function that applies the other functions when a new option is chosen
 function optionChanged(sample_data){
     filterMeta(sample_data);
     buildCharts(sample_data);
 };
-
+// Build our charts
 function buildCharts(sample_data){
     d3.json("samples.json").then(function(data){
     var otu_ids = data.otu_ids;
@@ -64,8 +65,24 @@ function buildCharts(sample_data){
       hovermode: 'closest',
       xaxis: {title: 'OTU ID'},
     };
-
+    // Attempt to plot bubble chaert
     Plotly.plot('bubble', bubData, Layout);
+
+    var sample_value = data.samples[0].sample_values;
+    var Labels = data.samples[0].otu_labels;
+    var slice = otu_ids.slice(0,10);
+
+    // Build Bar chart
+    var trace1 = {
+        x: sample_value,
+        y: slice,
+        text: Labels,
+        type: "bar",
+        orientation: "h"
+      }
+      data = [trace1];
+      // Attempt to plot bar chart
+      Plotly.newPlot("bar", data);
 
 })};
 init();
